7e7b6ce5921862a4fb2542f8d4fafab0548d7fc6
merajHossain <mh1669101@gmail.com>
2024-02-14 17:17:25 +0600

    remove order from orders

--- index.js
+++ index.js
@@ -72,6 +72,14 @@ async function run() {
       res.send(result);
     })
 
+    // for delete the orders data
+    app.delete('/checkOut/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await checkOutCollection.deleteOne(query);
+      res.send(result); 
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
